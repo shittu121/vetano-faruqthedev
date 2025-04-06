e5ffadfa3fb2cf0b7879c9bfb72c205912546140
shittu121 <shittufaruq6@gmail.com>
2025-04-06 14:42:57 +0100

    remove ' added &apos;

--- src/components/ui/success.tsx
+++ src/components/ui/success.tsx
@@ -25,7 +25,7 @@ export function SuccessComponent({ onClose }: Props) {
         <CardContent className="flex flex-col gap-2 p-8 border border-white/10">
           <CardTitle>Video Uploaded Successfully!</CardTitle>
           <CardDescription>
-          Your skill showcase video is now live. You've taken a powerful step toward standing out — employers can now see your talent in action before the first interview. Keep building your career with confidence!
+          Your skill showcase video is now live. You&apos;ve taken a powerful step toward standing out — employers can now see your talent in action before the first interview. Keep building your career with confidence!
           </CardDescription>
         </CardContent>
         <AiButton />
