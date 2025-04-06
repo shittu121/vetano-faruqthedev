08a0aa7c2124c4976b48dbedff53de529646a992
shittu121 <shittufaruq6@gmail.com>
2025-04-06 14:41:48 +0100

    ignore Elsint error

--- src/components/Preview-Video.tsx
+++ src/components/Preview-Video.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import VideoShowcase from "@/components/ui/video-player";
 import { NavBar } from "./navbar";
 
 const PreviewVideo = () => {
